13850239bad55adb5c440012731d6d0cc5e1dfaa
frangoni <fran.goni96@gmail.com>
2021-04-17 19:19:19 -0300

    Velas y ordenes autorizadas

--- bot.js
+++ bot.js
@@ -2,96 +2,46 @@ require("dotenv").config();
 const axios = require("axios");
 const crypto = require("crypto");
 const { apiKey, apiSecret } = process.env;
-const apiEP = "https://api.binance.com/";
+const apiEP = "https://api.binance.com/api";
 
 const signature = (query) => {
-  return `&signature=${crypto.createHmac("sha256", apiSecret).update(query).digest("hex")}`;
+  return crypto.createHmac("sha256", apiSecret).update(query).digest("hex");
 };
 
-const timestamp = () => `&timestamp=${Date.now()}`;
+const api = axios.create({
+  baseURL: apiEP,
+  headers: { "X-MBX-APIKEY": apiKey },
+});
 
-const log = async () => {
+const getMyOrders = async (symbol) => {
   try {
-    const query = "api/v3/allOrders&symbol=ETHUSDT";
-    const orderss = await axios.get(`${apiEP}${query}${signature(query)}${timestamp()}`, {
-      headers: { "X-MBX-APIKEY": apiKey },
+    const timestamp = Date.now();
+    const query = `timestamp=${timestamp}&symbol=${symbol}`;
+    const orderss = await api.get("/v3/allOrders", {
+      params: { timestamp, symbol, signature: signature(query) },
     });
     console.log(orderss);
   } catch (error) {
+    console.log("error");
+    console.log(error.response);
+  }
+};
+
+const eth = "ETHUSDT";
+const rsr = "RSRUSDT";
+
+// getMyOrders(rsr);
+
+const getCandles = async (symbol, interval) => {
+  try {
+    const candles = await api.get("/v3/klines", {
+      params: { interval, symbol, startTime: new Date().setHours(new Date().getHours() - 1) },
+    });
+    console.log(candles.data.length);
+  } catch (error) {
+    console.log("error");
     console.log(error);
   }
 };
 
-log();
-
-const tick = async (config, binanceClient) => {
-  const { asset, base, allocation, spread } = config;
-  const market = `${asset}/${base}`;
-  try {
-    //CONSEGUIR TODAS LAS ORDENES
-
-    const orders = await binanceClient.openOrders({ symbol: asset + base });
-
-    //CERRAR TODAS LAS ORDENES
-    orders.forEach(async (order) => {
-      await binanceClient.cancelOrder({ symbol: asset + base, orderId: order.id });
-    });
-
-    //TIPOS DE CAMBIO
-    const results = await Promise.all([
-      axios.get("https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"),
-      axios.get("https://api.coingecko.com/api/v3/simple/price?ids=tether&vs_currencies=usd"),
-    ]);
-
-    //BALANCES
-    /* 
-    const balances = await binanceClient.fetchBalance(); */
-    const assetBalance = balances.free[asset];
-    const baseBalance = balances.free[base];
-
-    //PRECIOS
-    const marketPrice = results[0].data.bitcoin.usd / results[1].data.tether.usd;
-    const sellPrice = marketPrice * (1 + spread);
-    const buyPrice = marketPrice * (1 - spread);
-
-    //VOLUMENES
-    const sellVolume = assetBalance * allocation;
-    const buyVolume = (baseBalance * allocation) / marketPrice;
-
-    console.log(binanceClient.allOrders());
-
-    //CREACION DE ORDENES
-    /*   await binanceClient.createLimitSellOrder(market, sellVolume, sellPrice);
-    await binanceClient.createLimitBuyOrder(market, buyVolume, buyPrice); */
-
-    console.log(`
-    New tick for ${market}...
-    Created limit sell order for ${sellVolume}@${sellPrice}
-    Created limit buy order for ${buyVolume}@${buyPrice}
-    `);
-  } catch (error) {
-    console.log("ERROR: " + error);
-  }
-};
-
-const bot = async () => {
-  const config = {
-    asset: "ETH",
-    base: "USDT",
-    //SI ES DE 0.1 DEBERIA HABER MAS DE 100 DOLS EN CUENTA (MINIMO USDT10 PARA OPERAR)
-    allocation: 0.1,
-    spread: 0.2,
-    tickInterval: 2000,
-  };
-
-  const binanceClient = new Binance().options({
-    APIKEY: apiKey,
-    APISECRET: apiSecret,
-    useServerTime: true,
-  });
-  console.log(await binanceClient.time());
-  tick(config, binanceClient);
-  setInterval(tick, config.tickInterval, config, binanceClient);
-};
-
-bot();
+getCandles(rsr, "5m");
